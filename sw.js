18cbfbefd90bd697efe4edeb33d2208367ef82ef
fikrisudra <fikrisudra@gmail.com>
2025-09-09 22:43:33 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,17 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("myproduction-cache").then((cache) =>
-      cache.addAll([
-        "index.html",
-        "dashboard.html",
-        "profile.html",
-        "404.html",
-        "style.css",
-        "manifest.json",
-        "icons/icon-192.png",
-        "icons/icon-512.png"
-      ])
-    )
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Langsung aktif
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('activate', (event) => {
+  self.clients.claim(); // Ambil kontrol langsung
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => new Response(
+        '<h1>Offline access is not allowed.</h1>',
+        { status: 503, headers: { 'Content-Type': 'text/html' } }
+      ))
   );
 });
